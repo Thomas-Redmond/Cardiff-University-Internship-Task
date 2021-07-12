99471f03360db86f31827bb8e65df1f6b1e7c06e
Thomas <LeaRedmondT@cardiff.ac.uk>
2021-07-12 15:08:55 +0100

    Flake8 tutorial always fail check

---
+++ flake8_COMSC_PSP.py
@@ -0,0 +1,23 @@
+import ast
+import sys
+from typing import Generator
+from typing import Tuple
+from typing import Type
+from typing import Any
+
+if sys.version_info < (3, 8):
+    import importlib_metadata
+else:
+    import importlib.metadata as importlib_metadata
+
+
+class Plugin:
+    # display plugin information in help messaging
+    name = __name__
+    version = importlib_metadata.version(__name__)
+
+    def __init__(self, tree: ast.AST) -> None:
+        self._tree = tree
+
+    def run(self) -> Generator[Tuple[int, int, str, Type[Any]], None, None]:
+        yield 1, 0, 'FNA1000 named argument', type(self)
