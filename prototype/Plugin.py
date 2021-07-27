0f16536d9454d7f70896193d1b146c6e018f84d0
Thomas <LeaRedmondT@cardiff.ac.uk>
2021-07-27 14:29:25 +0100

    Added instances to Plugin.py

--- prototype/Plugin.py
+++ prototype/Plugin.py
@@ -6,12 +6,22 @@ if sys.version_info < (3, 8):
 else:
     import importlib.metadata as importlib_metadata
 
+import AST_Router
+import reportError
+
 class Plugin:
     name = __name__
     version = importlib_metadata.version(__name__)
 
     def __init__(self, tree: ast.AST):
+        """
+
+        """
+
         self._tree = tree
+        self._reportError = reportError()
+        self._routerAST = AST_Router(self._reportError)
+        self._testUnit = Unit_Testing(self._reportError)
 
     def run(self):
         """
