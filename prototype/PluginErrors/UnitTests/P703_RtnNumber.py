79531fc59378f23a511f8a6ac7baab91ccdd7099
Thomas <LeaRedmondT@cardiff.ac.uk>
2021-07-27 15:38:45 +0100

    Moved documents to PluginErrors where it made sense so './tests' can be used for unit testing

---
+++ prototype/PluginErrors/UnitTests/P703_RtnNumber.py
@@ -0,0 +1,14 @@
+"""
+
+winProbability function must return number.
+Positive result if Float / Integer returned.
+Default = Fail
+
+"""
+
+import Squash
+
+def test_isFloat():
+    return
+
+def test_isInteger():
--- prototype/tests/return.py
+++
@@ -1,1 +0,0 @@
-def f(): return 1
