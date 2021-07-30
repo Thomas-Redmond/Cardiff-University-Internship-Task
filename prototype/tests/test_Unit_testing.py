f0aedc457b117cc287592c8625d595ff9f3ad719
Thomas <LeaRedmondT@cardiff.ac.uk>
2021-07-30 11:58:28 +0100

    added to tests checking setRecord function

--- prototype/tests/test_Unit_testing.py
+++ prototype/tests/test_Unit_testing.py
@@ -5,3 +5,8 @@ def test_init():
     r = reportError.Reporter()
     u = Unit_Testing.Controller(r)
     assert u._reportHere == r
+
+def test_var_testsToRun():
+    r = reportError.Reporter()
+    u = Unit_Testing.Controller(r)
+    assert u._testsToRun == []
