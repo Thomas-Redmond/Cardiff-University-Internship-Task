0cde18782c886d02997d04d3c878c45ec8120078
Thomas <LeaRedmondT@cardiff.ac.uk>
2021-08-24 16:49:12 +0100

    installation working again

--- src/Linter.py
+++ src/Linter.py
@@ -6,14 +6,40 @@ if sys.version_info < (3, 8):
 else:
     import importlib.metadata as importlib_metadata
 
+from pathlib import Path
 from typing import Generator
 from typing import Tuple
 from typing import Type
 from typing import Any
 
-import .errorReporter
-import AST_Router
-import Unit_Testing
+def addSRC2Path():
+    try:
+        fileDirectory = Path(__file__).parent
+        fileDirectoryParts = fileDirectory.parts
+        if 'lib' in fileDirectoryParts:
+            indexLib = fileDirectoryParts.index('lib')
+            newPath = Path()
+
+            for addressComponent in fileDirectoryParts:
+                if addressComponent != 'lib':
+                    newPath = newPath.joinpath(addressComponent)
+                else:
+                    newPath = newPath.parent # strip last directory before lib out of address
+                    print(newPath)
+                    break
+
+        # else:
+        #     raise Exception("Virtual Environment not in use")
+        sys.path.insert(0, str(newPath)) # add installation folder to path
+        print(f"Added file location {newPath} to Path")
+    except ModuleNotFoundError:
+        print(f"Importing source files failed using path {newPath} given {Path(__file__).parent}")
+        print(f"Traceback {e}")
+
+addSRC2Path()
+from src import errorReporter
+from src import AST_Router
+from src import Unit_Testing
 
 class Plugin:
     name = __name__
