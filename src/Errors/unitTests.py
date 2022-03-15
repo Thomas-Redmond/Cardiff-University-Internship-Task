2bb89639a6d02be59de5b7c3054b07fea7c24e48
Thomas <LeaRedmondT@cardiff.ac.uk>
2022-03-15 21:05:56 +0000

    created unitTests version capable of changing functions it is used on

--- src/Errors/unitTests.py
+++ src/Errors/unitTests.py
@@ -15,11 +15,11 @@ class TestCases:
             for row in reader:
                 # testID, in, out
 
-                testID = row[0]
-                testType = row[1]
-                input = row[2]
-                output = row[3]
+                testID = row[0]     # Get id of test
+                testType = row[1]   # Get type of test allowed values: type, value
+                func = row[2]       # Get function to test
 
+                input = row[-2]
                 if len(input) == 0:
                     input = None
                 elif input[0] == "[":
@@ -29,16 +29,18 @@ class TestCases:
                 else:
                     input = self.parseSingleValue(input)
 
+                # Get outputs from data
+                # Maximum of one only, if no output expected value recieved should be None
+                output = row[-1]
                 if len(output) == 0:
                     output = None
                 elif output[0] == "[":
                     # input designated as list
                     output = self.parseMultipleValues(output[1:-1])
-
                 else:
                     output = self.parseSingleValue(output)
 
-                self.testData.append([testID, testType, input, output])
+                self.testData.append([testID, testType, func, input, output])
 
         argumentParser = parser() # checks whether FileToTest address has been given correctly
         self.TestMe = argumentParser.FileToTest  # raises exception if not
@@ -72,20 +74,27 @@ class TestCases:
         # Errors will be recorded in the instance of Reporter passed to this class upon instantiation.
         try:
             for test in self.testData:
+                #try:
                 # If input does not match expected output
+                    errorCode = test[0]
+                    type = test[1].lower()
+                    func = test[2]
+                    # can be easier to access from rear due to adding new variables in database
+                    input = test[-2]
+                    expected = test[-1]
 
-                errorCode = test[0]
-                type = test[1].lower()
-                # can be easier to access from rear due to adding new variables in database
-                input = test[-2]
-                expected = test[-1]
+                    func = getattr(self.TestMe, func) # use function name as passed in data
 
-                if type == 'value':
-                    self.runValueTests(errorCode, input, expected)
-                elif type == 'type':
-                    self.runTypeTests(errorCode, input, expected)
-                else:
-                    print(f"{type} value not valid")
+                    if type == 'value':
+                        self.runValueTests(errorCode, func, input, expected)
+                    elif type == 'type':
+                        self.runTypeTests(errorCode, func, input, expected)
+                    else:
+                        print(f"{type} value not valid")
+
+                #except Exception as e:
+                #    print(f"There is a configuration fault with test {test[0]}")
+                #    print(f"{e}")
 
 
                 #if (self.TestMe.program(input) != expected):
@@ -97,13 +106,20 @@ class TestCases:
 
         return
 
-    def runValueTests(self, errorCode, input, expected):
-        if self.TestMe.program(input) != expected:
+    def runValueTests(self, errorCode, func, input, expected):
+        # Run value type tests
+        # If given multiple input values need to activate function to tests using *kwargs
+
+        if func(input) != expected:
             errorText = f"Input {type(input)} {input} Expected Output {type(expected)} {expected}"
             self.errorRecord.setRecord(-1, -1, errorCode + " : " + errorText)
 
-    def runTypeTests(self, errorCode, input, expected):
+    def runTypeTests(self, errorCode, func, input, expected):
+        # Run value type tests
+        # If given multiple input values need to activate function to tests using *kwargs
 
+
+        # Making sure expected type values have been written correctly
         if expected.lower() == 'list':
             expected = "<class 'list'>"
 
@@ -113,7 +129,6 @@ class TestCases:
         elif expected.lower() == 'None':
             expected = "<class 'NoneType'>"
 
-
-        if str(type(self.TestMe.program(input))) != expected:
+        if str(type(func(input))) != expected:
             errorText = f"Input {input} Expected Output {expected}"
             self.errorRecord.setRecord(-1, -1, errorCode + " : " + errorText)
