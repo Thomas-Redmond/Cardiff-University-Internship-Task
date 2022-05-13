8eba1e34f7f533a53e40d43bf1be18aee7de3355
Thomas <LeaRedmondT@cardiff.ac.uk>
2022-05-13 19:53:43 +0100

    IT WORKS AGAIN !!!

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,6 @@
 from .errorReporter import Reporter
 from .sysArgParser import parser
-from .Unit_Testing import Controller
+#from .Unit_Testing import Controller
 
 from .Errors.functionCheck import funcCheck
 from .Errors.unitTests import TestCases
